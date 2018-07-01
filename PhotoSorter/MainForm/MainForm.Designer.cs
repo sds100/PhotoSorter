1c60dfc1f80fe8e448996ce548f05bdeb0060b87
Seth Schroeder <seth.d.schroeder@gmail.com>
2018-07-01 16:21:25 +0100

    MainForm: Improve design

--- PhotoSorter/MainForm/MainForm.Designer.cs
+++ PhotoSorter/MainForm/MainForm.Designer.cs
@@ -36,20 +36,26 @@
             this.buttonSort = new System.Windows.Forms.Button();
             this.progressBar = new System.Windows.Forms.ProgressBar();
             this.labelVersion = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelProgress = new System.Windows.Forms.Label();
+            this.labelMessage = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // labelSourceDirectory
             // 
             this.labelSourceDirectory.AutoSize = true;
-            this.labelSourceDirectory.Location = new System.Drawing.Point(115, 12);
+            this.labelSourceDirectory.Location = new System.Drawing.Point(16, 122);
             this.labelSourceDirectory.Name = "labelSourceDirectory";
-            this.labelSourceDirectory.Size = new System.Drawing.Size(22, 13);
+            this.labelSourceDirectory.Padding = new System.Windows.Forms.Padding(0, 10, 0, 0);
+            this.labelSourceDirectory.Size = new System.Drawing.Size(22, 23);
             this.labelSourceDirectory.TabIndex = 0;
             this.labelSourceDirectory.Text = "C:\\";
             // 
             // buttonSourceDirectory
             // 
-            this.buttonSourceDirectory.Location = new System.Drawing.Point(519, 12);
+            this.buttonSourceDirectory.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSourceDirectory.Location = new System.Drawing.Point(432, 117);
             this.buttonSourceDirectory.Name = "buttonSourceDirectory";
             this.buttonSourceDirectory.Size = new System.Drawing.Size(140, 23);
             this.buttonSourceDirectory.TabIndex = 1;
@@ -59,25 +65,30 @@
             // 
             // labelOutputDirectory
             // 
+            this.labelOutputDirectory.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
             this.labelOutputDirectory.AutoSize = true;
-            this.labelOutputDirectory.Location = new System.Drawing.Point(12, 41);
+            this.labelOutputDirectory.Location = new System.Drawing.Point(16, 172);
             this.labelOutputDirectory.Name = "labelOutputDirectory";
-            this.labelOutputDirectory.Size = new System.Drawing.Size(22, 13);
+            this.labelOutputDirectory.Padding = new System.Windows.Forms.Padding(0, 10, 0, 0);
+            this.labelOutputDirectory.Size = new System.Drawing.Size(22, 23);
             this.labelOutputDirectory.TabIndex = 2;
             this.labelOutputDirectory.Text = "C:\\";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Location = new System.Drawing.Point(13, 103);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.Size = new System.Drawing.Size(148, 13);
             this.label1.TabIndex = 3;
-            this.label1.Text = "photos to organise:";
+            this.label1.Text = "Where are the photos to sort?";
             // 
             // buttonOutputDirectory
             // 
-            this.buttonOutputDirectory.Location = new System.Drawing.Point(519, 42);
+            this.buttonOutputDirectory.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonOutputDirectory.Location = new System.Drawing.Point(432, 172);
             this.buttonOutputDirectory.Name = "buttonOutputDirectory";
             this.buttonOutputDirectory.Size = new System.Drawing.Size(140, 23);
             this.buttonOutputDirectory.TabIndex = 4;
@@ -87,7 +98,8 @@
             // 
             // buttonSort
             // 
-            this.buttonSort.Location = new System.Drawing.Point(483, 200);
+            this.buttonSort.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSort.Location = new System.Drawing.Point(497, 246);
             this.buttonSort.Name = "buttonSort";
             this.buttonSort.Size = new System.Drawing.Size(75, 23);
             this.buttonSort.TabIndex = 5;
@@ -97,25 +109,60 @@
             // 
             // progressBar
             // 
-            this.progressBar.Location = new System.Drawing.Point(210, 300);
+            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar.Location = new System.Drawing.Point(16, 224);
             this.progressBar.Name = "progressBar";
-            this.progressBar.Size = new System.Drawing.Size(466, 23);
+            this.progressBar.Size = new System.Drawing.Size(556, 16);
             this.progressBar.TabIndex = 7;
             // 
             // labelVersion
             // 
+            this.labelVersion.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.labelVersion.AutoSize = true;
-            this.labelVersion.Location = new System.Drawing.Point(13, 425);
+            this.labelVersion.Location = new System.Drawing.Point(13, 251);
             this.labelVersion.Name = "labelVersion";
             this.labelVersion.Size = new System.Drawing.Size(45, 13);
             this.labelVersion.TabIndex = 8;
             this.labelVersion.Text = "Version:";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 159);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(191, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Where should the photos be sorted to?";
+            // 
+            // labelProgress
+            // 
+            this.labelProgress.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelProgress.AutoSize = true;
+            this.labelProgress.Location = new System.Drawing.Point(16, 205);
+            this.labelProgress.Name = "labelProgress";
+            this.labelProgress.Size = new System.Drawing.Size(49, 13);
+            this.labelProgress.TabIndex = 10;
+            this.labelProgress.Text = "Sorting...";
+            // 
+            // labelMessage
+            // 
+            this.labelMessage.AutoSize = true;
+            this.labelMessage.Location = new System.Drawing.Point(13, 13);
+            this.labelMessage.Name = "labelMessage";
+            this.labelMessage.Size = new System.Drawing.Size(184, 13);
+            this.labelMessage.TabIndex = 11;
+            this.labelMessage.Text = "You can sort both photos and videos.";
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(584, 281);
+            this.Controls.Add(this.labelMessage);
+            this.Controls.Add(this.labelProgress);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.labelVersion);
             this.Controls.Add(this.progressBar);
             this.Controls.Add(this.buttonSort);
@@ -124,8 +171,9 @@
             this.Controls.Add(this.labelOutputDirectory);
             this.Controls.Add(this.buttonSourceDirectory);
             this.Controls.Add(this.labelSourceDirectory);
+            this.MaximizeBox = false;
             this.Name = "MainForm";
-            this.Text = "Form1";
+            this.Text = "Photo Sorter";
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -142,6 +190,9 @@
         private System.Windows.Forms.Button buttonSort;
         private System.Windows.Forms.ProgressBar progressBar;
         private System.Windows.Forms.Label labelVersion;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelProgress;
+        private System.Windows.Forms.Label labelMessage;
     }
 }
 
