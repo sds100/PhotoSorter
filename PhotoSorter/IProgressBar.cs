4db2e7bc5502424684d2a5a8f38781cfa21b222f
Seth Schroeder <seth.d.schroeder@gmail.com>
2018-07-03 10:19:33 +0100

    IProgressBar: add OnStatusChange() method

--- PhotoSorter/IProgressBar.cs
+++ PhotoSorter/IProgressBar.cs
@@ -2,6 +2,11 @@
 {
     public interface IProgressBar
     {
+        /// <summary>
+        /// Show a message describing what is currently happening
+        /// </summary>
+        void OnStatusChange(string message);
+        
         void OnReportProgress(int percent);
         void OnProgressCompleted();
     }
